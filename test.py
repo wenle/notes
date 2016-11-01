350c177f16043e7a8656f9c6cd280eccb9b25425
wenle <baowenle@gmail.com>
2016-11-01 18:11:06 +0800

    temp add script

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import urllib
+import urllib2
+#url = "http://beijinglawyers.chinalawinfo.com/case/Result.asp?SFlag=11"
+#values = {"Para1Type" : "1", "HidPara5IDs" : "11,13", "otherPara6":"0401", "Range" : "1", "doSearch.x" : "41", "doSearch.y" : "10"}
+url = "http://beijinglawyers.chinalawinfo.com/case/Result.asp?SFlag=13&Para1Type=1&DispLeft=0&DispWord=&CheckFlag=0&KeyWord="
+values = {"page" : "10", "PageSelect" : "9", "preWhere" : "48%28%7C7B%522B%3D%25%23%30%30%32%25%23%25%2C%25%23%30%30%33%25%23%25%2C%25%23%30%30%34%25%23%25%29%20%41%4E%44%20%28%28%6570%636E%6765%6E90%3D%25%23%62%30%32%37%23%25%29%20%4F%52%20%28%6570%636E%6765%6E90%3D%25%23%62%30%32%38%23%25%29%29"}
+data = urllib.urlencode(values)
+print data
+req = urllib2.Request(url, data)
+response = urllib2.urlopen(req)
+the_page = response.read()
+print the_page
